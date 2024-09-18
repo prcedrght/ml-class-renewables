a4ee9e06c99d38bf46456bec6667af580169f726
dillonp <dillonp@netflix.com>
2024-09-17 20:55:10 -0600

    reduced time series file size

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73c9c568960ded7b5ec72f0856e1934dcfdb8a6a03dfcf724ba24a2f68b32a7c
-size 10996
+oid sha256:ceab2059d67dae58eb724f7fdafa8b76213092818e4cf727eafbf880ac224240
+size 11005
